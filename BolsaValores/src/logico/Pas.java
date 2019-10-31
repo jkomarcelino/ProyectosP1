7be3a7a9ab27fe6dfccdb37a05a01446482e543a
ApolinarUrena <44011030+ApolinarUrena@users.noreply.github.com>
2019-10-31 15:33:41 -0400

    sets y gets

--- BolsaValores/src/logico/Pas.java
+++ BolsaValores/src/logico/Pas.java
@@ -3,7 +3,25 @@ package logico;
 public class Pas {
 	int var;
 	String nombre;
-	String apellido; 
+	String apellido;
+	public int getVar() {
+		return var;
+	}
+	public void setVar(int var) {
+		this.var = var;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	} 
 	
 
 }
