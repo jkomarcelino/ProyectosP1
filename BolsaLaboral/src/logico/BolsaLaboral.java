9d40c79acd53df73cf9f252d74f11134237e802d
ApolinarUrena <44011030+ApolinarUrena@users.noreply.github.com>
2019-11-14 15:44:32 -0400

    Agregada clase solicPerson, modificada solicCentro y clase controladora.

--- BolsaLaboral/src/logico/BolsaLaboral.java
+++ BolsaLaboral/src/logico/BolsaLaboral.java
@@ -1,5 +1,47 @@
 package logico;
 
-public class BolsaLaboral {
+import java.util.ArrayList;
 
+public class BolsaLaboral {
+	private ArrayList<SolicCentro> listSolisC;
+	private ArrayList<SolicPerson> listSolisP;
+	private ArrayList<Personal> listPersonal;
+	private ArrayList<Empresa> listEmpresa;
+	
+	
+	public BolsaLaboral(ArrayList<SolicCentro> listSolisC, ArrayList<SolicPerson> listSolisP,
+			ArrayList<Personal> listPersonal, ArrayList<Empresa> listEmpresa) {
+		super();
+		this.listSolisC = listSolisC;
+		this.listSolisP = listSolisP;
+		this.listPersonal = listPersonal;
+		this.listEmpresa = listEmpresa;
+	}
+	
+	
+	public ArrayList<SolicCentro> getListSolisC() {
+		return listSolisC;
+	}
+	public void setListSolisC(ArrayList<SolicCentro> listSolisC) {
+		this.listSolisC = listSolisC;
+	}
+	public ArrayList<SolicPerson> getListSolisP() {
+		return listSolisP;
+	}
+	public void setListSolisP(ArrayList<SolicPerson> listSolisP) {
+		this.listSolisP = listSolisP;
+	}
+	public ArrayList<Personal> getListPersonal() {
+		return listPersonal;
+	}
+	public void setListPersonal(ArrayList<Personal> listPersonal) {
+		this.listPersonal = listPersonal;
+	}
+	public ArrayList<Empresa> getListEmpresa() {
+		return listEmpresa;
+	}
+	public void setListEmpresa(ArrayList<Empresa> listEmpresa) {
+		this.listEmpresa = listEmpresa;
+	}
+	
 }
--- BolsaLaboral/src/logico/SolicCentro.java
+++ BolsaLaboral/src/logico/SolicCentro.java
@@ -1,5 +1,97 @@
 package logico;
 
-public class SolicCentro {
+import java.util.ArrayList;
 
+public class SolicCentro {
+	private String idSoliC;
+	private ArrayList<Empresa> listEmpresa;
+	private String nivel;
+	private int annoExp;
+	private boolean vehiculo;
+	private boolean disp_viajar;
+	private boolean licencia;
+	private String sector;
+	private float sueldo;
+	private String tipoSueldo;
+	
+	
+	public SolicCentro(String idSoliC, ArrayList<Empresa> listEmpresa, String nivel, int annoExp, boolean vehiculo,
+			boolean disp_viajar, boolean licencia, String sector, float sueldo, String tipoSueldo) {
+		super();
+		this.idSoliC = idSoliC;
+		this.listEmpresa = listEmpresa;
+		this.nivel = nivel;
+		this.annoExp = annoExp;
+		this.vehiculo = vehiculo;
+		this.disp_viajar = disp_viajar;
+		this.licencia = licencia;
+		this.sector = sector;
+		this.sueldo = sueldo;
+		this.tipoSueldo = tipoSueldo;
+	}
+	
+	
+	
+	public String getIdSoliC() {
+		return idSoliC;
+	}
+	public void setIdSoliC(String idSoliC) {
+		this.idSoliC = idSoliC;
+	}
+	public ArrayList<Empresa> getListEmpresa() {
+		return listEmpresa;
+	}
+	public void setListEmpresa(ArrayList<Empresa> listEmpresa) {
+		this.listEmpresa = listEmpresa;
+	}
+	public String getNivel() {
+		return nivel;
+	}
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+	public int getAnnoExp() {
+		return annoExp;
+	}
+	public void setAnnoExp(int annoExp) {
+		this.annoExp = annoExp;
+	}
+	public boolean isVehiculo() {
+		return vehiculo;
+	}
+	public void setVehiculo(boolean vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+	public boolean isDisp_viajar() {
+		return disp_viajar;
+	}
+	public void setDisp_viajar(boolean disp_viajar) {
+		this.disp_viajar = disp_viajar;
+	}
+	public boolean isLicencia() {
+		return licencia;
+	}
+	public void setLicencia(boolean licencia) {
+		this.licencia = licencia;
+	}
+	public String getSector() {
+		return sector;
+	}
+	public void setSector(String sector) {
+		this.sector = sector;
+	}
+	public float getSueldo() {
+		return sueldo;
+	}
+	public void setSueldo(float sueldo) {
+		this.sueldo = sueldo;
+	}
+	public String getTipoSueldo() {
+		return tipoSueldo;
+	}
+	public void setTipoSueldo(String tipoSueldo) {
+		this.tipoSueldo = tipoSueldo;
+	}
+	
+	
 }
