eee8fd2ee89ffb805972cc48f62def72132a2acf
ApolinarUrena <44011030+ApolinarUrena@users.noreply.github.com>
2019-11-22 02:26:26 -0400

    cambio estructura visual principal

--- BolsaLaboral/src/visual/Principal.java
+++ BolsaLaboral/src/visual/Principal.java
@@ -14,6 +14,11 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JMenu;
+import javax.swing.border.BevelBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.FlowLayout;
+import javax.swing.border.TitledBorder;
 
 public class Principal extends JFrame {
 
@@ -40,13 +45,39 @@ public class Principal extends JFrame {
 		setResizable(false);
 		setTitle("Bolsa Laboral");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1048, 707);
+		setBounds(100, 100, 1072, 729);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		getContentPane().add(panel, BorderLayout.SOUTH);
+		getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_1.setBounds(10, 641, 1046, 27);
+		panel.add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_2.setBounds(10, 78, 1048, 563);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_3.setBounds(531, 11, 507, 265);
+		panel_2.add(panel_3);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_4.setBounds(10, 287, 511, 265);
+		panel_2.add(panel_4);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_5.setBounds(531, 287, 507, 265);
+		panel_2.add(panel_5);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
