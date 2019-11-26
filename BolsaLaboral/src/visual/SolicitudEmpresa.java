722d8fb0795f2a5e3652264eb610b68c87ef385f
Usuario <Usuario@DESKTOP-EJHCJ3D>
2019-11-26 11:34:34 +0100

    solicitud empresa (no terminada)

--- BolsaLaboral/src/visual/SolicitudEmpresa.java
+++ BolsaLaboral/src/visual/SolicitudEmpresa.java
@@ -14,10 +14,14 @@ import javax.swing.UIManager;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.Panel;
+import java.awt.ScrollPane;
+import javax.swing.JRadioButton;
+import java.awt.Choice;
 
 public class SolicitudEmpresa extends JDialog {
-
-	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -36,21 +40,14 @@ public class SolicitudEmpresa extends JDialog {
 	 * Create the dialog.
 	 */
 	public SolicitudEmpresa() {
+		setTitle("Solicitud Empresa");
 		setBounds(100, 100, 600, 385);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new BorderLayout(0, 0));
-		{
-			JPanel panel = new JPanel();
-			panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Solicitud Empresa", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-			contentPanel.add(panel, BorderLayout.CENTER);
-			panel.setLayout(null);
-		}
+		getContentPane().setLayout(null);
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBounds(0, 303, 582, 35);
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			getContentPane().add(buttonPane);
 			{
 				JButton okButton = new JButton("Enviar");
 				okButton.addActionListener(new ActionListener() {
@@ -67,5 +64,51 @@ public class SolicitudEmpresa extends JDialog {
 				buttonPane.add(cancelButton);
 			}
 		}
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informaci\u00F3n General", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBounds(12, 13, 558, 119);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblCdigo = new JLabel("C\u00F3digo Empresa");
+		lblCdigo.setBounds(12, 33, 109, 16);
+		panel.add(lblCdigo);
+		
+		textField = new JTextField();
+		textField.setBounds(116, 30, 116, 22);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNombre = new JLabel("Nombre Empresa");
+		lblNombre.setBounds(12, 75, 109, 16);
+		panel.add(lblNombre);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(126, 72, 116, 22);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(276, 33, 56, 16);
+		panel.add(label);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(null, "Nivel Ac\u00E1demico", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBounds(12, 135, 558, 70);
+		getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Universitario");
+		rdbtnNewRadioButton.setBounds(38, 24, 107, 25);
+		panel_1.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnTcnico = new JRadioButton("T\u00E9cnico");
+		rdbtnTcnico.setBounds(200, 24, 73, 25);
+		panel_1.add(rdbtnTcnico);
+		
+		JRadioButton rdbtnObrero = new JRadioButton("Obrero");
+		rdbtnObrero.setBounds(360, 24, 73, 25);
+		panel_1.add(rdbtnObrero);
 	}
 }
